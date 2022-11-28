dc34ecc17f4ae389a692713051ad867fb614d22d
JoshNarindra <jbhawanlall@gmail.com>
2022-11-28 22:30:27 +0000

    Fixed bug - always found in array.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
+/*
+23/11/22 - NOVUS FS SOFTWARE DEVELOPER PROGRAMME
+Exercise 2:
+Create programme called ExistInArray.
+Create, Initialise, Pre-fill array with values of choosing.
+Ask User to enter an integer value to search for in array.
+If Value exists, output "Found in Array" - vice versa
+ */
+
 
 //Import necessary libraries
 import java.util.Scanner;
